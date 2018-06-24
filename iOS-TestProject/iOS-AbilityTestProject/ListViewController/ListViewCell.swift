4add5b6709e422dea3a0ddd040620891bd605499
炫迈口香糖 <370120333@qq.com>
2018-06-24 13:24:21 +0800

    fix

--- iOS-TestProject/iOS-AbilityTestProject/ListViewController/ListViewCell.swift
+++ iOS-TestProject/iOS-AbilityTestProject/ListViewController/ListViewCell.swift
@@ -17,10 +17,6 @@ class ListViewCell: UITableViewCell {
     @IBOutlet weak var describtion: UILabel!
     @IBOutlet weak var imageHrefView: UIImageView!
     
-    @IBOutlet weak var imageViewHeight: NSLayoutConstraint!
-    
-    @IBOutlet weak var describtionLableWidth: NSLayoutConstraint!   //xib添加约束时图片和描述label等宽（可更改）
-    
     weak var cellModel:ListViewRowModel? {
         willSet(newValue){
             
@@ -29,7 +25,6 @@ class ListViewCell: UITableViewCell {
         didSet{
             title.text = cellModel?.title
             describtion.text = cellModel?.description
-            imageHrefView.kf.setImage(with: URL(string: (cellModel?.imageHref) ?? "default"), placeholder: UIImage(named: "default"))
             
             imageHrefView.kf.setImage(with: URL(string: (cellModel?.imageHref) ?? "default"), placeholder: UIImage(named: "default")) { (image, _, _, _) in
                 //获取下载图片的宽高等
